c6f4a2544386f8c0f78e1c544f58f34575613bc4
meganr <meganrm@gmail.com>
2017-12-03 15:29:06 -0800

    added index.html

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const database = require('./postToInd/townHallListener');
 
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
+});
+
 server.start(app, PORT)
   .then(console.log)
   .then(database)
