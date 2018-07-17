f0783a84ea79a7c84a7c3ed3724b794028d75ff2
meganrm <meganrm@gmail.com>
2018-07-17 16:17:52 -0700

    added test for has been changed method

---
+++ pull_from_indivisible/tests/group.test.js
@@ -0,0 +1,36 @@
+const mockGroup = require('./mockGroup');
+const Group = require('../group');
+
+describe('group module', () => {
+  describe('has been changed', () => {
+    test('if nothing has been changed returns false', () => {
+      let groupInFirebase = new Group(mockGroup);
+      let newGroup = new Group(mockGroup);
+      let hasBeenChanged = newGroup.hasBeenChanged(groupInFirebase);
+      expect(hasBeenChanged).toEqual(false);
+    });
+    test('if the city has changed returns true', () => {
+      let groupInFirebase = new Group(mockGroup);
+      let newGroup = new Group(mockGroup);
+      newGroup.city = 'New City';
+      let hasBeenChanged = newGroup.hasBeenChanged(groupInFirebase);
+      expect(hasBeenChanged).toEqual(true);
+    });
+
+    test('if a value gets added that was not there returns true', () => {
+      let groupInFirebase = new Group(mockGroup);
+      groupInFirebase.zip = null;
+      let newGroup = new Group(mockGroup);
+      let hasBeenChanged = newGroup.hasBeenChanged(groupInFirebase);
+      expect(hasBeenChanged).toEqual(true);
+    });
+
+    test('if a value gets deleted returns true', () => {
+      let groupInFirebase = new Group(mockGroup);
+      let newGroup = new Group(mockGroup);
+      newGroup.zip = null;
+      let hasBeenChanged = newGroup.hasBeenChanged(groupInFirebase);
+      expect(hasBeenChanged).toEqual(true);
+    });
+  });
+});
---
+++ pull_from_indivisible/tests/mockGroup.js
@@ -0,0 +1,92 @@
+module.exports = {
+  'address': {
+    'city': 'City',
+    'country': null,
+    'postal_code': '06840',
+    'state': 'CT',
+    'street': null,
+  },
+  'assignee_id': 218649,
+  'contact_type_id': 547508,
+  'custom_fields': [{
+    'custom_field_definition_id': 109096,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 109116,
+    'value': 140251,
+  },
+  {
+    'custom_field_definition_id': 109334,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 125880,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 169681,
+    'value': 242147,
+  },
+  {
+    'custom_field_definition_id': 170811,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 173127,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 173129,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 102127,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 135034,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 125881,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 182617,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 117296,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 109338,
+    'value': null,
+  },
+  {
+    'custom_field_definition_id': 135035,
+    'value': [],
+  },
+  {
+    'custom_field_definition_id': 117297,
+    'value': null,
+  },
+  ],
+  'date_created': 1492084062,
+  'date_modified': 1515449095,
+  'details': null,
+  'email_domain': null,
+  'id': 12033851,
+  'interaction_count': 15,
+  'name': 'Test Group',
+  'phone_numbers': [],
+  'socials': [{
+    'category': 'facebook',
+    'url': 'https://www.facebook.com/',
+  }],
+  'tags': [
+    'ct-04',
+  ],
+  'websites': [],
+};
