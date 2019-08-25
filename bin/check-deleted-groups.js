266186b8c76fb0f3cc7ef50d32bb807aeaae3ff9
meganrm <meganrm@gmail.com>
2019-08-24 21:21:02 -0700

    change detete groups to full read

--- bin/check-deleted-groups.js
+++ bin/check-deleted-groups.js
@@ -2,29 +2,41 @@
 const firebasedb = require('../lib/setup-indivisible-firebase');
 const getOneGroup = require('../pull_from_indivisible/get-one-group');
 const Group = require('../pull_from_indivisible/group');
-const startingGroup = 12029911;
-const endingGroup = 31611321 - 5297;
-const startPoint = Math.floor(Math.random() * (endingGroup - startingGroup + 1) + startingGroup);
 
-firebasedb.ref('indivisible_groups').orderByKey().startAt(`${startPoint}`).limitToFirst(5297).once('value').then(snapshot => {
-  let index = 0;
-  console.log(startPoint);
-  snapshot.forEach((res) => {
-    let group = res.val();
-    let id = group.id;
-    index++;
-    const requestOneGroupAndDeleteRemoved = () => {
-      getOneGroup(id)
-        .then((returned) => {
-          if (returned === 'Not Found') {
-            console.log('removing', id);
-            Group.remove();
-          } else if (!returned.id) {
-            console.log(`Other server error when looking for group: ${index} ${returned}`);
-          }
-        });
-    };
-    setTimeout(requestOneGroupAndDeleteRemoved, index * 300);
- 
+const ref = firebasedb.ref('indivisible_groups');
+ref.once('value')
+  .then(snapshot => {
+    let index = 0;
+    const total = snapshot.numChildren();
+    console.log('total', total);
+
+    snapshot.forEach((res) => {
+      let group = res.val();
+      let groupId = group.id;
+      index++;
+
+      const requestOneGroupAndDeleteRemoved = (id) => {
+        if (id == 12031594) {
+          console.log('found 12031594');
+        }
+        if (id == 12030566) {
+          console.log('found 12030566');
+        }
+        getOneGroup(id)
+          .then((returned) => {
+            if (returned === 'Not Found') {
+              console.log('removing', id);
+              Group.remove(id);
+            } else if (!returned.id) {
+              console.log(`Other server error when looking for group: ${index} ${returned}`);
+            }
+          });
+      };
+      setTimeout(() => requestOneGroupAndDeleteRemoved(groupId), index * 300);
+    });
+    setTimeout(() => {
+      console.log('done');
+      process.exit(0);
+    }, (total + 1) * 300);
   });
-});
+
