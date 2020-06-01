a8e84d8e7b4044fe0a932566c39f3e25d8981030
meganrm <meganrm@gmail.com>
2020-05-31 23:49:36 -0700

    adding status to properties

--- pull_from_indivisible/point.js
+++ pull_from_indivisible/point.js
@@ -13,7 +13,7 @@ class Point {
       twitter: group.twitter || false,
       facebook: group.facebook || false,
       title: group.title || group.name,
-      active: group.active,
+      status: group.status,
       id: group.id || false,
     };
   }
