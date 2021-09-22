02d6c4beea2e1bb27f3ceaa4eedffd3e6d129d27
Jason <JasonW94@gmail.com>
2021-09-22 15:28:01 -0500

    Log group JSON to IN S3 (#21)
    Upload group map geo json to Indivisible s3 for debugging purposes.

--- pull_from_indivisible/mapboxUpload.js
+++ pull_from_indivisible/mapboxUpload.js
@@ -20,12 +20,30 @@ const uploadToMapbox = (url) => {
     });
 };
 
+// Upload the group data to Indivisible's S3
+const uploadToIndivisible = (geoJSON) => {
+  console.log("Uploading to IN s3");
+  var inS3 = new AWS.S3({
+    accessKeyId: process.env.IN_S3_KEY_ID,
+    secretAccessKey: process.env.IN_S3_SECRET_KEY
+  });
+  s3.putObject({
+    Bucket: process.env.IN_S3_BUCKET,
+    Key: process.env.IN_S3_KEY,
+    Body: JSON.stringify(geoJSON)
+  }, function(err) {
+    console.log("Posted geo json to IN s3");
+    console.log(err);
+  });
+};
+
 module.exports = (geoJSON) => {
   return superagent
     .post(`${mapboxapi}/${process.env.MAPBOX_USERNAME}/credentials?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`)
     .then(res => {
       const credentials = res.body;
       console.log(credentials);
+      uploadToIndivisible(geoJSON);
       // Use aws-sdk to stage the file on Amazon S3
       var s3 = new AWS.S3({
         accessKeyId: credentials.accessKeyId,
