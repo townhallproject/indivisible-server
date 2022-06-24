7971532dd06918124a2e7a69b3f79411f50fbdc0
Jason Walker <jason@indivisible.org>
2022-06-24 11:29:44 -0500

    Note skipped mobilize events

--- pull_from_indivisible/event.js
+++ pull_from_indivisible/event.js
@@ -57,6 +57,7 @@ class IndEvent {
 
     // DEBUG: mobilize events are causing problems. Let's only upload our test event for now.
     if (this.campaignNo === MOBILIZE_CAMPAIGN_ID && this.id !== 166481) {
+        console.log("DEBUG Skipping mobilize evente with id ", this.id);
         return;
     }
 
